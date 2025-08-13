0e7ce7ee61ad78ef79b434736a6a3633d6e7d9a3
xxc8889 <jiangzong811@gmail.com>
2025-08-13 21:46:31 +0800

    Delete TelegramMonitor-1.20250805.0220 directory

--- TelegramMonitor-1.20250805.0220/src/Models/DisplayDialogs.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TelegramMonitor;
-
-public class DisplayDialogs
-{
-    public long Id { get; set; }
-    public string DisplayTitle { get; set; }
-}
--- TelegramMonitor-1.20250805.0220/src/Models/TelegramMonitorConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TelegramMonitor;
-
-public static class TelegramMonitorConstants
-{
-    public const string MonitorApi = "https://raw.githubusercontent.com/Riniba/TelegramMonitor/refs/heads/main/ad/ad.txxt";
-    public const int ApiId = 23319500;
-    public const string ApiHash = "814ac0dd67f660119b9b990d514c9a47";
-    public static readonly string SessionPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "session");
-}
